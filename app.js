961d5ce434bbb44aa6521d668ef42545afc05b67
himani1717 <himanisharma17.2003@gmail.com>
2022-07-09 11:41:22 +0530

    update

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+let mysql = require('mysql');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/student');
+var teacherRouter = require('./routes/teacher');
+var vserverRouter = require('./routes/vserver');
 
 var app = express();
-
 // view engine setup
 // app.use(express.static('views',{extensions:['html']}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 // app.engine('html',require('hogan-express'));
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,9 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/student', studentRouter);
+app.use('/teacher', teacherRouter);
+app.use('/vserver', vserverRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
